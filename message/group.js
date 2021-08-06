6d714a7f79d2c6a6c5d1253ea6155ac6ecfa864a
MurphyBot <86679476+MurphyBot@users.noreply.github.com>
2021-08-07 00:58:10 +0700

    Add files via upload

--- message/group.js
+++ message/group.js
@@ -2,16 +2,14 @@ const {
 	MessageType
 } = require("@adiwajshing/baileys");
 const fs = require("fs-extra")
-const moment = require("moment-timezone")
 
 const { getBuffer } = require('../lib/myfunc')
 const { color, bgcolor } = require('../lib/color')
 
 let setting = JSON.parse(fs.readFileSync('./setting.json'))
-prefix = setting.prefix
 
 module.exports = welcome = async (nino, anu) => {
-	    const welkom = JSON.parse(fs.readFileSync('./database/welcome.json'))
+	    const welkom = JSON.parse(fs.readFileSync('./database/group/welcome.json'))
 	    const isWelcome = welkom.includes(anu.jid)
 	    if (!isWelcome) return
 		try {
@@ -28,7 +26,7 @@ module.exports = welcome = async (nino, anu) => {
                 pp_grup = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
             }
             if (anu.action == 'add' && mem.includes(nino.user.jid)) {
-            nino.sendMessage(anu.jid, `Halo! Terima Kasih sudah Mengundangku, Jika ingin Menggunakan Bot Ketik ${prefix}menu`, 'conversation')
+            nino.sendMessage(anu.jid, 'Halo! Terima Kasih sudah Mengundangku, Jika ingin Menggunakan Bot Ketik !menu', 'conversation')
             }
              if (anu.action == 'add' && !mem.includes(nino.user.jid)) {
              if (!welkom.includes(anu.jid)) return
@@ -37,9 +35,8 @@ module.exports = welcome = async (nino, anu) => {
             	num = anu.participants[0]
                 let v = nino.contacts[num] || { notify: num.replace(/@.+/, '') }
                 anu_user = v.vname || v.notify || num.split('@')[0]
-                time_wel = moment.tz('Asia/Jakarta').format("HH:mm")
-                teks = `Halo ${anu_user} \n\nNama : \nUmur :\nGender : \nAsal :\n\nSemoga Betah dan jangan lupa isi\nAnd Following Rules Group\n${mdata.desc}`
-	            buff = await getBuffer(`https://hardianto-chan.herokuapp.com/api/tools/welcomer2?name=${encodeURI(anu_user)}&descriminator=${time_wel}&totalmem=${memeg}&namegb=${encodeURI(mdata.subject)}&ppuser=${pp_user}&background=https://telegra.ph/file/9c41931cada0cfe68b8b7.jpg&apikey=hardianto`)
+                teks = `Halo ${anu_user} \n\nNama : \nUmur :\nGender : \nAsal :\n\nSemoga Betah dan jangan lupa isi`
+	            buff = await getBuffer(`http://hadi-api.herokuapp.com/api/card/welcome?nama=${num.split('@')[0]}&descriminator=${memeg}&memcount=${memeg}&gcname=${encodeURI(mdata.subject)}&pp=${pp_user}&bg=https://telegra.ph/file/841db869a5bb2cced77c9.jpg`)
 		        nino.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
 		}
             if (anu.action == 'remove' && !mem.includes(nino.user.jid)) {
@@ -48,10 +45,9 @@ module.exports = welcome = async (nino, anu) => {
             	num = anu.participants[0]
                 let w = nino.contacts[num] || { notify: num.replace(/@.+/, '') }
                 anu_user = w.vname || w.notify || num.split('@')[0]
-                time_wel = moment.tz('Asia/Jakarta').format("HH:mm")
                 memeg = mdata.participants.length
                 out = `Kenapa tuh? kok bisa keluar? \nSayonara ${anu_user} we will miss you`
-                buff = await getBuffer(`https://hardianto-chan.herokuapp.com/api/tools/leave2?name=${encodeURI(anu_user)}&descriminator=${time_wel}&totalmem=${memeg}&namegb=${encodeURI(mdata.subject)}&ppuser=${pp_user}&background=https://telegra.ph/file/9c41931cada0cfe68b8b7.jpg&apikey=hardianto`)
+                buff = await getBuffer(`http://hadi-api.herokuapp.com/api/card/goodbye?nama=${num.split('@')[0]}&descriminator=${memeg}&memcount=${memeg}&gcname=${encodeURI(mdata.subject)}&pp=${pp_user}&bg=https://telegra.ph/file/0de9e05e9ebaf276b848e.jpg`)
                 nino.sendMessage(mdata.id, buff, MessageType.image, {caption: out, contextInfo: {"mentionedJid": [num]}})
             }
 		} catch (e) {
